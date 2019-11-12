ea4b674d19711f14c12ead6a000403d078bbeaec
Temirkhan Myrzamadi <gymmasssorla@gmail.com>
2019-11-13 03:11:25 +0600

    Add comments to http_server_multithreaded_pinned.c

--- http_server_multithreaded_pinned.c
+++ http_server_multithreaded_pinned.c
@@ -1,3 +1,35 @@
+/*
+ * Версия многопоточного сервера, где потоки прибиты к ядрам. Производительность
+ * почти такая же, как и у http_server_multithreaded.c:
+ *
+ * $ wrk -c100 -d1m -t8 http://127.0.0.1:18470 -H "Host: 127.0.0.1:18470" \
+ *      -H "Accept-Language: en-US,en;q=0.5" -H "Connection: keep-alive"
+ * Running 1m test @ http://127.0.0.1:18470
+ *  8 threads and 100 connections
+ *  Thread Stats   Avg      Stdev     Max   +/- Stdev
+ *    Latency   495.55us    1.00ms  25.84ms   89.62%
+ *    Req/Sec    79.39k    31.63k  159.24k    66.97%
+ *  37914962 requests in 1.00m, 5.19GB read
+ * Requests/sec: 631227.47
+ * Transfer/sec:     88.49MB
+ *
+ * ------------------------------
+ *
+ * $ sudo perf stat ./http_server_multithreaded_pinned
+ * Performance counter stats for './http_server_multithreaded_pinned':
+ *
+ *     241364,468945      task-clock (msec)         #    3,996 CPUs utilized
+ *         2 015 452      context-switches          #    0,008 M/sec
+ *                12      cpu-migrations            #    0,000 K/sec
+ *               244      page-faults               #    0,001 K/sec
+ *   886 526 857 409      cycles                    #    3,673 GHz
+ *   617 732 545 050      instructions              #    0,70  insn per cycle
+ *   119 311 277 122      branches                  #  494,320 M/sec
+ *     3 285 806 736      branch-misses             #    2,75% of all branches
+ *
+ *      60,396719472 seconds time elapsed
+ */
+
 #include <stdbool.h>
 
 #define _GNU_SOURCE
